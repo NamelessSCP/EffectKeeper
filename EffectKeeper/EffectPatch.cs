18ea0196b4609b39b0b8fa6a05b37ef163b464da
Misfiy <85962933+Misfiy@users.noreply.github.com>
2025-02-18 20:45:47 +0100

    LABAPI LETS GO

--- EffectKeeper/EffectPatch.cs
+++ EffectKeeper/EffectPatch.cs
@@ -1,59 +1,55 @@
-﻿namespace EffectKeeper 
-{
-    using CustomPlayerEffects;
-    using HarmonyLib;
-    using PlayerRoles;
+﻿namespace EffectKeeper;
+
+using CustomPlayerEffects;
+using HarmonyLib;
+using PlayerRoles;
     
 #if EXILED
     using Exiled.API.Enums;
     using Exiled.API.Extensions;
 #endif
     
-    [HarmonyPatch(typeof(PlayerEffectsController), nameof(PlayerEffectsController.OnRoleChanged))]
-    public static class EffectPatch
+[HarmonyPatch(typeof(PlayerEffectsController), nameof(PlayerEffectsController.OnRoleChanged))]
+public static class EffectPatch
+{
+    private static Config config => EffectKeeper.Instance.Config;
+
+    private static bool Prefix(
+        PlayerEffectsController __instance,
+        ReferenceHub targetHub,
+        PlayerRoleBase oldRole,
+        PlayerRoleBase newRole)
     {
-        private static Config config => EffectKeeper.Instance.Config;
+        if (targetHub != __instance._hub)
+            return false;
 
-        private static bool Prefix(
-            PlayerEffectsController __instance,
-            ReferenceHub targetHub,
-            PlayerRoleBase oldRole,
-            PlayerRoleBase newRole)
+        bool isDeath = config.DeathCancels && (oldRole.Team == Team.Dead || newRole.Team == Team.Dead);
+
+        foreach (StatusEffectBase statusEffectBase in __instance.AllEffects)
         {
-            if (targetHub != __instance._hub)
-                return false;
-
-            bool isDeath = config.DeathCancels && (oldRole.Team == Team.Dead || newRole.Team == Team.Dead);
-
-            foreach (StatusEffectBase statusEffectBase in __instance.AllEffects)
+            if (isDeath)
             {
-                if (isDeath)
-                {
-                    statusEffectBase.OnDeath(oldRole);
-                    continue;
-                }
-
-                bool shouldCancel = false;
-
-#if EXILED
-                if (!statusEffectBase.TryGetEffectType(out EffectType effectType))
-                    continue;
-
-                EffectCategory categories = effectType.GetCategories();
-                if (!config.AllowedEffects.Contains(effectType) && !config.AllowedCategories .Any(c => categories.HasFlag(c)))
-                    shouldCancel = true;
-#else
-                if (!config.AllowedEffects.Contains(statusEffectBase.name))
-                    shouldCancel = true;
-                
-                if (!config.AllowedCategories.Contains(statusEffectBase.Classification))
-                    shouldCancel = true;
-#endif
-                if (shouldCancel)
-                    statusEffectBase.OnRoleChanged(oldRole, newRole);
+                statusEffectBase.OnDeath(oldRole);
+                continue;
             }
 
-            return false;
+            bool shouldCancel = false;
+
+#if EXILED
+            if (!statusEffectBase.TryGetEffectType(out EffectType effectType))
+                continue;
+
+            EffectCategory categories = effectType.GetCategories();
+            if (!config.AllowedEffects.Contains(effectType) && !config.AllowedCategories .Any(c => categories.HasFlag(c)))
+                shouldCancel = true;
+#else
+            shouldCancel = !config.AllowedCategories.Contains(statusEffectBase.Classification)
+                           && !config.AllowedEffects.Contains(statusEffectBase.name);
+#endif
+            if (shouldCancel)
+                statusEffectBase.OnRoleChanged(oldRole, newRole);
         }
+
+        return false;
     }
 }
